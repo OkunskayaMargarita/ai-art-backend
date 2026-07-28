46b39fe28027c7ab73aff13eaac069d51042b1f2
BillyNogami72 <vsevolod.markin.2001@gmail.com>
2026-07-29 00:28:13 +0200

    Update size limits

--- models/profile.py
+++ models/profile.py
@@ -6,8 +6,8 @@ class ProfileData(BaseModel):
     base_positive: str = "masterpiece, best quality, score_7, no censor, no watermark, no text"
     base_negative: str = "worst quality, low quality, score_1, score_2, score_3, artist name, blurry, jpeg artifacts, lowres, censor"
 
-    width: int = Field(default=768, ge=64, le=2048)
-    height: int = Field(default=768, ge=64, le=2048)
+    width: int = Field(default=768, ge=64, le=4096)
+    height: int = Field(default=768, ge=64, le=4096)
 
     steps: int = Field(default=10, ge=1, le=50)
     cfg: float = Field(default=1.0, ge=0.0, le=30.0)
